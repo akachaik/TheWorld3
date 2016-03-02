8171a8bd870c75488e378b0c5ba29ab7a6a0d210
kit <308tube@gmail.com>
2016-03-03 00:41:00 +0700

    add bootstrap and font awesome

--- src/TheWorld3/wwwroot/js/site.js
+++ src/TheWorld3/wwwroot/js/site.js
@@ -5,10 +5,13 @@
 
     $("#sidebarToggle").on("click", function() {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
+        var $icon = $("#sidebarToggle i.fa");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.addClass("fa-angle-right");
+            $icon.removeClass("fa-angle-left");
         } else {
-            $(this).text("Hide Sidebar");
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
         }
     });
     
