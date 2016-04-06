ff89c5c3d2d3e7591d09071bba9fffc8c63c0aa5
akachai.k@taskworld.com <akachai.k@taskworld.com>
2016-04-06 23:12:22 +0700

    Design the Login View

---
+++ src/TheWorld3/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace TheWorld3.Controllers
+{
+    public class  AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld3/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheWorld3.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
